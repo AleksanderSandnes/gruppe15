b5735a76b97d8aadbcbac79b7fdc00b7e8e4512d
Ranum99 <aleksanderranum99@hotmail.com>
2020-01-21 20:22:58 +0100

    Update sendMessageToTeacher.php

--- oblig1/php/sendMessageToTeacher.php
+++ oblig1/php/sendMessageToTeacher.php
@@ -5,12 +5,12 @@
    $teacherId = $_POST['teacher'];
    $melding = $_POST['message'];
 
-   if (!empty($teacherId) || !empty($fag)) {
+   if (!empty($teacherId) || !empty($melding)) {
         $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname);
         if (mysqli_connect_error()) {
             die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
         } else {
-            $INSERT = "INSERT INTO melding (idBrukerFra, melding, idFag) VALUES (?, ?, ?)";
+            $INSERT = "INSERT INTO melding (idBrukerFra, melding, idFag, upassende, status) VALUES (?, ?, ?, 0, 0)";
 
             $stmt = $conn->prepare($INSERT);
             $stmt->bind_param("isi", $login_id, $melding, $teacherId);
