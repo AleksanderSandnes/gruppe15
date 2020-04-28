8f92a099b06752ca2750619c91e0d98464e01386
Sandnes <Aleksander.sandnes@hotmail.com>
2020-04-28 19:31:04 +0200

    Update approveTeacher.php

--- oblig1/php/approveTeacher.php
+++ oblig1/php/approveTeacher.php
@@ -1,4 +1,25 @@
 <?php
+    // Composer autoloader
+    require __DIR__ . '../vendor/autoload.php';
+
+    // Shortcuts for simpler usage
+    use Monolog\Logger;
+    use Monolog\Formatter\LineFormatter;
+    use Monolog\Handler\StreamHandler;
+
+    // Common Logger
+    $Log = new Logger('log-files');
+
+    // Line formatter without empty brackets in the end
+    $formatter = new LineFormatter(null, null, false, true);
+
+    // Information level handler
+    $informationHandler = new StreamHandler('../logs/information.log', Logger::INFO);
+    $informationHandler->setFormatter($formatter);
+
+    // This will INFORMATION messages
+    $Log->pushHandler($informationHandler);
+
    include('inputValidation.php');
    include('cookiemonster.php');
 
@@ -15,6 +36,10 @@
           $stmtsql = $conn->prepare($sql);
           $stmtsql->bind_param("i",$brukerID);
           $stmtsql->execute();
+
+          // Logger at en bruker er godkjent
+          $Log->info("En bruker har blitt godkjent.", ['Brukerid'=>$brukerID]);
+
           echo "Bruker godkjent";
        }
    } else {
